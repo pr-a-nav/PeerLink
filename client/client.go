9c3b7963967de014da8a1fd0986b172cfe0cf036
pr-a-nav <pranavmishra680@gmail.com>
2024-08-01 21:47:23 +0530

    client directory created

---
+++ client/client.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "net"
+)
+
+func main() {
+	
+	var seradd string = "localhost:8080"
+    conn, err := net.Dial("tcp", seradd)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    res , err := conn.Write([]byte(" wanna join your server"))
+    if err != nil {
+        fmt.Println(err)
+    }
+	
+
+    conn.Close()
+}
