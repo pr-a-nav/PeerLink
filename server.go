684650a1fa3530bfd77424e41f6786d028f18164
vishwas-12 <bishwasvikram.singh2002@gmail.com>
2024-10-06 10:23:37 +0530

    added setupOrbitDB in server.go

--- server.go
+++ server.go
@@ -4,7 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"net"
-
+	 orbitdb "github.com/orbitdb/go-orbit-db"
+	"github.com/ipfs/go-ipfs/core"
 	// "bufio"
 	// "context"
 	// "encoding/json"
@@ -13,30 +14,28 @@ import (
 	// "net/http"
 )
 
+var dbLock sync.Mutex
 
-type peerID  = map[string]string
+type peerID = map[string]string
 
-type Peer struct{
- Address string
- Port int
-
+type Peer struct {
+	Address string
+	Port    int
 }
+
 var groups = map[string][]Peer{
-	"SDE 1": {},
-	"Ml Guys":{},
-	"Torrent Guys":{},
+	"SDE 1":        {},
+	"Ml Guys":      {},
+	"Torrent Guys": {},
 }
 
+type RendezvousPoint struct {
+	identifier int
+	peerlist   []peerID
+	mu         sync.Mutex
+}
 
-
- type RendezvousPoint struct {
-	identifier  int
-	peerlist  []peerID
-	    mu   sync.Mutex
-
- }
- func  CollectDataFromIP(rp *RendezvousPoint, ipAddr string, data interface{}) error {
-	
+func CollectDataFromIP(rp *RendezvousPoint, ipAddr string, data interface{}) error {
 
 	// rp.peerlist[ipAddr] = data
 
@@ -48,113 +47,143 @@ var groups = map[string][]Peer{
 
 	return nil
 }
-func server(){
+func server() {
 	fmt.Println("server started")
-	ln ,err := net.Listen("tcp", ":9000")
-	 if err!=nil{
+	ln, err := net.Listen("tcp", ":9000")
+	if err != nil {
 		fmt.Println("error", err)
-	 }
+	}
 	for {
-		req ,err := ln.Accept()
-		if err!=nil{
+		req, err := ln.Accept()
+		if err != nil {
 			fmt.Println("error", err)
 		}
 		// reader  := bufio.NewReader(req)
 		// message ,err := reader.ReadString(byte(reader.Buffered()))
 		// fmt.Println(message)
 		list := "groups"
-       for  group := range groups{
-			list +=group+"\n"
+		for group := range groups {
+			list += group + "\n"
 		}
 		req.Write([]byte(list))
-		
+
 		handle(req)
 	}
 
 }
 
-func handle(conn net.Conn){
-  defer conn.Close()
-   res := New("12.45.56", 45)
-   
-   add := conn.RemoteAddr().(*net.TCPAddr)
-   clientIP := add.IP.String()
-   clientport := add.Port
-   register(res, clientIP, clientport)
-   reader  := bufio.NewReader(conn)
-   message ,err := reader.ReadString(byte(reader.Buffered()))
-   if err!=nil{
-	fmt.Println(err)
-   }
-   fmt.Println(message)
-//    var selected string 
-//    _ , err := fmt.Fscanln(conn, &selected)
-//    if err!=nil{
-// 	fmt.Print(err)
-//    }
-//    fmt.Println(selected)
-   fmt.Println("nopes")
-   fmt.Printf("IP %s port %d", clientIP,clientport)
-//    list := "groups"
-// 	for  group := range groups{
-// 		list +=group+"\n"
-// 	}
-// 	conn.Write([]byte(list))
-	
+func handle(conn net.Conn) {
+	defer conn.Close()
+	res := New("12.45.56", 45)
+
+	add := conn.RemoteAddr().(*net.TCPAddr)
+	clientIP := add.IP.String()
+	clientport := add.Port
+	register(res, clientIP, clientport)
+	reader := bufio.NewReader(conn)
+	message, err := reader.ReadString(byte(reader.Buffered()))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(message)
+	//    var selected string
+	//    _ , err := fmt.Fscanln(conn, &selected)
+	//    if err!=nil{
+	// 	fmt.Print(err)
+	//    }
+	//    fmt.Println(selected)
+	fmt.Println("nopes")
+	fmt.Printf("IP %s port %d", clientIP, clientport)
+	//    list := "groups"
+	// 	for  group := range groups{
+	// 		list +=group+"\n"
+	// 	}
+	// 	conn.Write([]byte(list))
 
 }
 
-func sendgroups(conn net.Conn){
+func sendgroups(conn net.Conn) {
 	list := "groups"
-	for  group := range groups{
-		list +=group+"\n"
+	for group := range groups {
+		list += group + "\n"
 	}
 	conn.Write([]byte(list))
 }
 
-func register(Ipadd Ipad, ip string , port int){
+func register(Ipadd Ipad, ip string, port int) {
 	Ipad.add(Ipadd, ip, port)
 
 }
-
 
 func vishwas() {
 	fmt.Println("hello pranav")
 }
 
-type Ipad struct{
-    ipad []string
+type Ipad struct {
+	ipad []string
 	port []int
 }
 
-type Group struct{
-   ID string
-   Name string
-   Members map[string]*Ipad
-   messages []Message
+type Group struct {
+	ID       string
+	Name     string
+	Members  map[string]*Ipad
+	messages []Message
 }
 
-type Message struct{
+type Message struct {
 	SenderID string
-	Content string
+	Content  string
 }
-
-
 
 func New(ip string, port int) Ipad {
-    return Ipad{
-        ipad:   []string{ip},  
-        port: []int{port}, 
-    }
+	return Ipad{
+		ipad: []string{ip},
+		port: []int{port},
+	}
 }
-func (i Ipad )add(ipad string, port int ){
-i.ipad = append(i.ipad,ipad )
-i.port = append(i.port, port)
+func (i Ipad) add(ipad string, port int) {
+	i.ipad = append(i.ipad, ipad)
+	i.port = append(i.port, port)
 
 }
+func setupOribitDB()(*core.IpfsNode, orbitdb.OrbitDB){
+	node,err:=ipfs.NewIPFSNode()
+	if err!=nil{
+		fmt.Print("Error in IPFS :",err)
+	}
 
-func main(){
+	db, err:=orbitdb.NewOrbitDB(node)
+
+	if err!=nil{
+		fmt.Print("Error initialising orbitdb:",err)
+	}
+	return node,db
+}
+func main() {
 	res := New("12.45.56", 45)
 	res.add("12.4.6", 45)
-	server()
+	node, db:=setupOribitDB()
+
+	listener,err:=net.Listen("tcp","localhost:9000")
+	if err!=nil{
+		fmt.Println("Error starting",err)
+		return
+	}
+
+	defer listener.Close()
+
+	fmt.Println(("Server is listening on host:9000"))
+
+	for{
+		conn,err:=listener.Accept()
+		if err!=nil{
+			fmt.Println(("error accepting connection:",err))
+		}
+		go handleClient(conn,db)
+	}
+
+	// server() included this fucntion in main itself
 }
+
+
